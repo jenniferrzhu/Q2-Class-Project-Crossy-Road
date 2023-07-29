3ba6696bc5588217d34068b4d735bf6c505c37ed
Jennifer Zhu <90798447+jenniferrzhu@users.noreply.github.com>
2023-07-29 16:55:35 -0700

    Create Car.java

---
+++ Q2 Project/src/Car.java
@@ -0,0 +1,71 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Car {
+	//variables that will be used
+	public double x, y;  //position of car
+	public double vx = 15.0;  //velocity of car
+	public double vy = 0.5; //velocity of car
+	private Image img; 	
+	private AffineTransform tx; 
+
+	public Car(double x, double y) { //constructor for making an object
+		this.x = x; 
+		this.y = y;
+		img = getImage("/imgs/carL.png"); 
+
+		tx = AffineTransform.getTranslateInstance(x, y );
+		init(x, y); 			 
+	}
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(x, y);
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+
+		//call update to update the actually picture location
+		update();
+		g2.drawImage(img, tx, null);
+		
+
+	}
+	
+	/* update the picture variable location */
+	private void update() {
+
+		x -= vx;
+		y -= vy;
+		tx.setToTranslation(x, y);
+		
+		//to scale it up or down to change size, .5 means 50% of original file
+		tx.scale(2.0, 2.0);
+
+	}
+
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		
+		//to scale it up or down to change size, .5 means 50% of original file
+		tx.scale(2.0, 2.0);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Chicken.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+}
